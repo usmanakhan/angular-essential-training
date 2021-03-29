d824bc2b4410e76912e43207bf9010ac26ce78c2
Usman <usman242@gmail.com>
2021-03-29 06:39:51 +0400

    adding interaction for favorite direcrtive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { MediaItemComponent } from './media-item.component';
 import { MediaItemListComponent } from './media-item-list.component';
+import { FavoriteDirective } from './favorite.directive';
+import { categoryListPipe } from './category-list.pipe';
 
 @NgModule({
   imports: [
-    BrowserModule
+  
+BrowserModule
   ],
   declarations: [
     AppComponent,
     MediaItemComponent,
-    MediaItemListComponent
+    MediaItemListComponent,
+    FavoriteDirective,
+    categoryListPipe
   ],
   bootstrap: [
     AppComponent
